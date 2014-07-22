f32ce8dae8a932e64cca58ca51faf1b0c2a8163a
kourosh hakhamaneshi <kouroshhakhamaneshi@kouroshs-MacBook-Pro.local>
2014-07-22 13:59:42 +0430

    Initial Commit

---
+++ Color Detection/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Color Detection
+//
+//  Created by kourosh hakhamaneshi on 7/22/14.
+//  Copyright (c) 2014 Kourosh Hakhamaneshi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
